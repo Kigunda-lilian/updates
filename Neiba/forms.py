c923e5ac20c24c1eadfec30f35b08eba554a9eb4
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-03-20 22:03:24 +0300

    create profile form

--- Neiba/forms.py
+++ Neiba/forms.py
@@ -1,2 +1,9 @@
 from django import forms
 from .models import *
+from django.forms import ModelForm
+from django.db.models import fields
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
