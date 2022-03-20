175869aae62b01338367897fa0089d43062936d7
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-03-20 19:29:42 +0300

    create Neighbourhood and Location model

--- Habari/settings.py
+++ Habari/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Neiba'
+    'Neiba',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Neiba/models.py
+++ Neiba/models.py
@@ -14,3 +14,16 @@ class Location(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=20)
+    photo = models.ImageField(upload_to = 'media/', null = True, blank = True)
+    about = models.TextField(max_length=200, null=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    occupants_count = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cop_dail = models.IntegerField(null=True, blank=True)
+    health_dail = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
