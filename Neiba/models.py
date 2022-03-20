8f88ebe0977fa1a32e8586753c305ef68965663e
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-03-20 19:17:47 +0300

    create Location model

--- Neiba/models.py
+++ Neiba/models.py
@@ -5,3 +5,12 @@ from PIL import Image
 
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=20)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def save_loc(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
