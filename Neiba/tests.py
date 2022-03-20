dcd1d1907c2e9b32dd59a395a230149639697b72
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-03-20 22:24:41 +0300

    create a business test instance

--- Neiba/tests.py
+++ Neiba/tests.py
@@ -3,3 +3,11 @@ from .models import *
 from django.contrib.auth.models import User
 
 # Create your tests here.
+class BusinessTestClass(TestCase):
+    def setUp(self):
+        self.business = Business(name='Coding Business')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.business, Business))
+
+    
