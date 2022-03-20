7427befad9a7c1a399429df64146b0a79089439d
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-03-20 22:38:44 +0300

    add save and delete method

--- Neiba/tests.py
+++ Neiba/tests.py
@@ -29,4 +29,13 @@ class LocationTestClass(TestCase):
     def test_instance(self):
         self.assertTrue(isinstance(self.location, Location))
 
-    
+    def test_save_method(self):
+        self.location.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+    def test_delete_method(self):
+        self.location.save_location()
+        self.location.delete()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) == 0)
