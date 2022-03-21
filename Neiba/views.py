cab3496e329926dae9ff178056e3191d27582861
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-03-21 11:19:47 +0300

    create neighbourhood views

--- Neiba/views.py
+++ Neiba/views.py
@@ -48,3 +48,50 @@ def update_profile(request, id):
             return redirect('profile')
 
     return render(request, 'user/update_prof.html', {"form": form})
+
+@login_required(login_url="/users/login/")
+def create_neighbourhood(request):
+    current_user = request.user
+    if request.method == 'POST':
+        hood_form = NeighbourhoodForm(request.POST, request.FILES)
+        if hood_form.is_valid():
+            hood = hood_form.save(commit=False)
+            hood.user = current_user
+            hood.save()
+        return HttpResponseRedirect('/neighbourhood')
+    else:
+        hood_form = NeighbourhoodForm()
+    context = {'hood_form': hood_form}
+    return render(request, 'main/create_hood.html', context)
+
+@login_required(login_url="/users/login/")
+def neighbourhood(request):
+    current_user = request.user
+    hood = Neighbourhood.objects.all().order_by('-id')
+    return render(request, 'main/neighbourhood.html', {'hood': hood, 'current_user': current_user})
+
+@login_required(login_url='/accounts/login/')
+def one_hood(request, name):
+    current_user = request.user
+    hood = Neighbourhood.objects.get(name=name)
+    profiles = Profile.objects.filter(hood=hood)
+    busineses = Business.objects.filter(hood=hood)
+    posts = Post.objects.filter(name=name)
+    request.user.profile.hood = hood
+    request.user.profile.save()
+
+    return render(request, 'main/one_hood.html', {'hood': hood, 'busineses': busineses, 'posts': posts, 'current_user': current_user, 'profiles': profiles})
+
+def join_hood(request, id):
+    hood = get_object_or_404(Neighbourhood, id=id)
+
+    request.user.profile.hood = hood
+    request.user.profile.save()
+    return redirect('neighbourhood')
+
+
+def leave_hood(request, id):
+    hood = get_object_or_404(Neighbourhood, id=id)
+    request.user.profile.hood = None
+    request.user.profile.save()
+    return redirect('neighbourhood')
