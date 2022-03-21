860692c4079f39355fdaab671d97a2d19dad75d0
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-03-21 11:03:28 +0300

    create uodate profile view

--- Neiba/views.py
+++ Neiba/views.py
@@ -11,3 +11,40 @@ def index(request):
     hood = Neighbourhood.objects.all().order_by('-name')
     return render(request, 'main/index.html')
 
+@login_required(login_url="/users/login/")
+def search(request):
+    if 'search_term' in request.GET and request.GET["search_term"]:
+        search_term = request.GET.get("search_term")
+        hoods_searched = Neighbourhood.objects.filter(
+            name__icontains=search_term)
+        message = f"Search For: {search_term}"
+
+        return render(request, "users/search.html", {"message": message, "neighbourhood": hoods_searched})
+    else:
+        message = "You haven't searched for any term"
+        return render(request, "search.html", {"message": message})
+    
+@login_required(login_url="/users/login/")
+def profile(request):
+    current_user = request.user
+    profile = Profile.objects.filter(user_id=current_user.id).first()
+    hood = Neighbourhood.objects.all()
+    businesses = Business.objects.filter(user_id=current_user.id)
+    posts = Post.objects.filter(user_id=current_user.id)
+    return render(request, "all-temps/profile.html", {"profile": profile, 'hood': hood, 'businesses': businesses, "posts": posts, })
+
+
+@login_required(login_url="/users/login/")
+def update_profile(request, id):
+    user = User.objects.get(id=id)
+    profile = Profile.objects.get(user_id=user)
+    form = UpdateProfileForm(instance=profile)
+    if request.method == "POST":
+        form = UpdateProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+
+            profile = form.save(commit=False)
+            profile.save()
+            return redirect('profile')
+
+    return render(request, 'user/update_prof.html', {"form": form})
