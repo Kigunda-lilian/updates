fd360c1a0064edaf991f00d77d272a14a95bc52b
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-03-21 08:59:05 +0300

    create homepage view

--- Neiba/views.py
+++ Neiba/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from .models import *
+from .forms import *
 
 # Create your views here.
+@login_required(login_url='/users/login/')
+def index(request):
+    hood = Neighbourhood.objects.all().order_by('-name')
+    return render(request, 'main/index.html')
+
